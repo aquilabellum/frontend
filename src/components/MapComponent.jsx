1000dde7ace052a25e4c1c730b9a53fa88934109
Flamur-Maliqi <flamur.maliqi@ibm.com>
2025-02-14 17:13:13 +0100

    added grid-layout over image

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,18 +1,37 @@
 import mapImage from "../assets/map.png";
+import friendlySoldier from "../assets/friendly-soldier.png";
 import "../styles/map.css";
 
 function MapComponent() {
-    return (
-        <div className="map-container">
-            <img src={mapImage} alt="Map" className="map-image" />
-            <div className="message-box">
-                <div className="message">
-                    <div className="message-text"> &gt;&gt; test Message at xyz</div>
-                    <div className="message-date"> 19:15</div>
-                </div>
-            </div>
+  const images = [
+    { src: friendlySoldier, x: 3, y: 3 },
+    { src: friendlySoldier, x: 5, y: 4 },
+  ];
+  return (
+    <div className="map-container">
+      <img src={mapImage} alt="Map" className="map-image" />
+      <div className="map-wrapper">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image.src}
+            className="overlay-image"
+            style={{
+              gridColumn: image.x,
+              gridRow: image.y,
+            }}
+          />
+        ))}
+      </div>
+
+      <div className="message-box">
+        <div className="message">
+          <div className="message-text"> &gt;&gt; test Message at xyz</div>
+          <div className="message-date"> 19:15</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default MapComponent;
