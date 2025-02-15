fbbe0fc8266aa5793246cf9c7a2a3ff37dce2ddf
Martin, Mohammed (ext) (CYS GRS ARC) <mohammed.martin.ext@siemens.com>
2025-02-15 02:22:55 +0100

    add connectivity to websocket server + make get request to api/initialState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,123 @@
-import './App.css'
-import CounterComponent from './components/CounterComponent'
-import HeaderComponent from './components/HeaderComponent'
-import MapComponent from './components/MapComponent'
-import TimelineComponent from './components/TimelineComponent'
+import "./App.css";
+import HeaderComponent from "./components/HeaderComponent";
+import MapComponent from "./components/MapComponent";
+import { WEBSOCKET_URL } from "./constants";
+import { io } from "socket.io-client";
+import { useEffect, useState, useCallback } from "react";
 
 function App() {
- return(
+  const [socket, setSocket] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [initialState, setInitialState] = useState(null);
+
+  const subscribe = useCallback(
+    (topic) => {
+      if (socket?.connected) {
+        console.log(`Subscribing to ${topic}`);
+        socket.emit("subscribe", [topic]);
+      }
+    },
+    [socket]
+  );
+
+  const unsubscribe = useCallback(
+    (topic) => {
+      if (socket?.connected) {
+        console.log(`Unsubscribing from ${topic}`);
+        socket.emit("unsubscribe", topic);
+      }
+    },
+    [socket]
+  );
+
+  useEffect(() => {
+    const newSocket = io(WEBSOCKET_URL, {
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+      reconnectionAttempts: Infinity,
+    });
+
+    setSocket(newSocket);
+
+    return () => {
+      if (newSocket) {
+        newSocket.disconnect();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const onConnect = () => {
+      console.log("Socket.IO connected");
+      setIsConnected(true);
+
+      const topics = [
+        "spawnEntity",
+        "detection",
+        "locationChanged",
+        "supportNeeded",
+        "dartStatusUpdate",
+      ];
+      topics.forEach((topic) => subscribe(topic));
+    };
+
+    const onDisconnect = () => {
+      console.log("Socket.IO disconnected");
+      setIsConnected(false);
+    };
+
+    const eventHandlers = {
+      dartStatusUpdate: (data) => console.log("dartStatusUpdate", data),
+      spawnEntity: (data) => console.log("spawnEntity", data),
+      detection: (data) => console.log("detection", data),
+      locationChanged: (data) => console.log("locationChanged", data),
+      supportNeeded: (data) => console.log("supportNeeded", data),
+    };
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    Object.entries(eventHandlers).forEach(([event, handler]) => {
+      socket.on(event, handler);
+    });
+
+    socket.onAny((event, ...args) => {
+      console.log(`Received event: ${event}`, args);
+    });
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      Object.keys(eventHandlers).forEach((event) => {
+        socket.off(event);
+      });
+      socket.offAny();
+    };
+  }, [socket, subscribe]);
+
+  useEffect(() => {
+    const fetchInitialState = async () => {
+      try {
+        const response = await fetch(`${WEBSOCKET_URL}/api/initialState`);
+        const data = await response.json();
+        console.log("Initial state:", data);
+        setInitialState(data);
+      } catch (error) {
+        console.error("Failed to fetch initial state:", error);
+      }
+    };
+    fetchInitialState();
+  }, []);
+
+  return (
     <div>
-      <HeaderComponent></HeaderComponent>
-      <MapComponent></MapComponent>
+      <HeaderComponent isConnected={isConnected} />
+      <MapComponent />
     </div>
- ) 
+  );
 }
 
-export default App
+export default App;
--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,39 +1,15 @@
-import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
 import mapImage from "../assets/map.png";
 import friendlyDrone from "../assets/friendly-drone.png";
 import dartIcon from "../assets/dart-icon.svg";
+
 import "../styles/map.css";
 
+const images = [
+  { src: friendlyDrone, x: 3, y: 3 },
+  { src: dartIcon, x: 5, y: 5 },
+];
+
 function MapComponent() {
-  
-  useEffect(() => {
-    const socket = io("http://localhost:3000");
-
-    socket.on("connect", () => {
-      console.log("Socket.IO connected");
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Socket.IO disconnected");
-    });
-
-    socket.onAny((event, ...args) => {
-        console.log(`Received event: ${event}`, args);
-        setMessages((prev) => [...prev, { event, data: args }]);
-    });
-
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  const images = [
-    { src: friendlyDrone, x: 3, y: 3 },
-    { src: dartIcon, x: 5, y: 5 }
-  ];
-
   return (
     <div className="map-container">
       <img src={mapImage} alt="Map" className="map-image" />
@@ -52,10 +28,10 @@ function MapComponent() {
       </div>
       <div className="gray-overlay"></div>
       <div className="message-box">
-          <div className="message">
-            <div className="message-text">&gt;&gt; testMessage</div>
-            <div className="message-date">19:15</div>
-          </div>
+        <div className="message">
+          <div className="message-text">&gt;&gt; testMessage</div>
+          <div className="message-date">19:15</div>
+        </div>
       </div>
     </div>
   );
